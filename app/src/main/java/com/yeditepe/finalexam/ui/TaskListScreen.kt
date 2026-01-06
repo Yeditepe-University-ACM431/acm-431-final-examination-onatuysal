0b7e89d0094e614d9c1f8e8f08ff6d55599c2bc6
onatuysal <onat.uysal@std.yeditepe.edu.tr>
2026-01-06 15:26:04 +0300

    Update TaskListScreen.kt

--- app/src/main/java/com/yeditepe/finalexam/ui/TaskListScreen.kt
+++ app/src/main/java/com/yeditepe/finalexam/ui/TaskListScreen.kt
@@ -14,10 +14,13 @@ import com.yeditepe.finalexam.viewmodel.TaskViewModel
 fun TaskListScreen(viewModel: TaskViewModel = viewModel()) {
 
     // TODO 3: Read task list from ViewModel
-
+val tasks = viewModel.tasks.value
     Column {
         // TODO 4: Display task titles and completion status
         // Use a simple Column or LazyColumn
+         tasks.forEach {task ->
+            TaskRow(task =task,navController =navController)
+         }
     }
 }
 
@@ -28,6 +31,7 @@ fun TaskRow(task: Task, navController: NavController) {
         text = task.title,
         modifier = Modifier.clickable {
             // TODO 3: Navigate to detail screen with task title
+            navController.navigate("taskDetail/${task.title}")
         }
     )
 }
