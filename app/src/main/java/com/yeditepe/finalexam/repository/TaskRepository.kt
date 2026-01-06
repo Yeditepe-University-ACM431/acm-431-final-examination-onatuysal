2cf379373f948121e2fe73c797cea330ce4df31c
onatuysal <onat.uysal@std.yeditepe.edu.tr>
2026-01-06 15:29:37 +0300

    Update TaskRepository.kt

--- app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
+++ app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
@@ -7,7 +7,14 @@ class TaskRepository(private val api: TaskApi) {
 
     suspend fun fetchTasks(): List<Task> {
         // TODO 1: Call API
+        val taskDtos =api.getTasks()
         // TODO 2: Convert TaskDto list to Task list
-        return TODO("Provide the return value")
+        return taskDtos.map{
+        Task(
+                id =it.id,
+                title =it.title,
+                isCompleted =it.completed
+            )
+        }
     }
 }
